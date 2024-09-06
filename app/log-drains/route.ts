82b7bfcc0decea439f80bdeb937ea7484abe1e5e
Tom Bremer <tom@tbremer.com>
2024-09-06 09:24:45 -0500

    init

---
+++ app/log-drains/route.ts
@@ -0,0 +1,16 @@
+import { STATUS_CODES } from "node:http";
+
+export function POST() {
+	if (!process.env.vercelVerify) {
+		return new Response(null, {
+			status: 500,
+			statusText: STATUS_CODES[500],
+		});
+	}
+
+	const headers = new Headers({
+		"x-vercel-verify": process.env.vercelVerify,
+	});
+
+	return new Response(null, { headers });
+}
